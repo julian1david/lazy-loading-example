2590b4dc6bc289bb3dbcf7db20ed233b0d6a1c83
JulianF <juliandavidbf@gmail.com>
2022-05-11 15:50:17 -0500

    @feat:update Readme

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
 
-console.log('Happy hacking :)')
+//create image
+import { registerImage } from "./lazy";
+import { createImagesNodes } from './utils'
+
+
+// Cargue las imagenes existentes cuando cargue la página
+const allImages = document.querySelectorAll("img[data-src]");
+allImages.forEach(registerImage);
+
+//Agregar las nuevas imagenes.
+const mountNode = document.querySelector('#images');
+const addButton = document.querySelector('#addImage');
+
+const addImage = () => 
+{ 
+    const [node, image] = createImagesNodes();
+    //guardamos la imagen que recibimos retornada de la funcion en una variable
+    registerImage(image);
+    //agregamos la imagen al contenedor padre
+    mountNode.append(node);
+}
+
+addButton.addEventListener('click', addImage);
+
+//Limpiar
+const clean = document.querySelector("button[type='reset']");
+clean.addEventListener("click", () => {
+    mountNode.innerHTML = "";
+});
